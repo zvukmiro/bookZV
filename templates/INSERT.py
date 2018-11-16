5bcce526b321e63251cbbede1c2d3444cea0c10c
Zorana <zvukmiro@gmail.com>
2018-11-16 12:45:08 -0800

    cleaned working version, no LIKE query used

--- templates/INSERT.py
+++
@@ -1,7 +0,0 @@
-    isbn= '0380795272'
-    username = "usernew2"
-    review = "5"
-    res = db.execute("INSERT INTO reviews (rw_isbn, rw_user, rating) VALUES (:bookNum, :person, :numb)", {"bookNum":isbn, "person":username, "numb":review})
-    db.commit()
-    #                           name of table(table rows names)             (some names any)      ( match names from VALUES() with variables defined before)
-
